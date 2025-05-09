ca73acac801e24646e6666f3e32d233be0e48b86
Darnivo <darnivo04@gmail.com>
2025-05-09 11:09:35 +0200

    sufficient v2?

--- starting_code/server/src/TCPServerSample.cs
+++ starting_code/server/src/TCPServerSample.cs
@@ -4,144 +4,147 @@ using System.Net;
 using System.Collections.Generic;
 using shared;
 using System.Threading;
+using System.Linq;
 
-/**
- * This class implements a simple tcp echo server.
- * Read carefully through the comments below.
- * Note that the server does not contain any sort of error handling.
- */
 class TCPServerSample
 {
 
-	// > new stuff for clients
-	class ClientState 
-	{
-		public TcpClient Client;
-		public int AvatarId;
-		public float X, Y, Z;
-		public int Skin;
-	}
+    // new client stuff
+    class ClientState
+    {
+        public TcpClient Client;
+        public int AvatarId;
+        public float X, Y, Z;  // Changed from Position
+        public int Skin;
+    }
 
-	private List<ClientState> _clients = new List<ClientState>();
-	private int _nextAvatarId = 0;
+    private List<ClientState> _clientStates = new List<ClientState>();
+    private int _nextAvatarId = 0;
+    private TcpListener _listener;
+    private List<TcpClient> _clients = new List<TcpClient>(); // Original client list
 
-	public static void Main(string[] args)
-	{
-		TCPServerSample server = new TCPServerSample();
-		server.run();
-	}
+    public static void Main(string[] args)
+    {
+        new TCPServerSample().run();
+    }
 
-	private TcpListener _listener;
-	private List<TcpClient> _clients = new List<TcpClient>();
+    private void run()
+    {
+        Console.WriteLine("Server started on port 55555");
+        _listener = new TcpListener(IPAddress.Any, 55555);
+        _listener.Start();
 
-	private void run()
-	{
-		Console.WriteLine("Server started on port 55555");
+        while (true)
+        {
+            processNewClients();
+            processExistingClients();
+            Thread.Sleep(100);
+        }
+    }
 
-		_listener = new TcpListener(IPAddress.Any, 55555);
-		_listener.Start();
-
-		while (true)
-		{
-			processNewClients();
-			processExistingClients();
-
-			//Although technically not required, now that we are no longer blocking, 
-			//it is good to cut your CPU some slack
-			Thread.Sleep(100);
-		}
-	}
-
-	private void processNewClients()
-	{
-		while (_listener.Pending())
-		{
-			_clients.Add(_listener.AcceptTcpClient());
-			Console.WriteLine("Accepted new client.");
-		}
-	}
+    private void processNewClients()
+    {
+        while (_listener.Pending())
+        {
+            TcpClient newClient = _listener.AcceptTcpClient();
+            _clients.Add(newClient);
+            _clientStates.Add(new ClientState { Client = newClient }); // Initialize ClientState
+            Console.WriteLine("Accepted new client.");
+        }
+    }
 
 
-	// > new processExistingClients()
-	private void processExistingClients() 
-	{
-		foreach (ClientState clientState in _clients.ToList()) 
-		{
-			NetworkStream stream = clientState.Client.GetStream();
-			if (stream.DataAvailable) 
-			{
-				byte[] bytes = StreamUtil.Read(stream);
-				Packet packet = new Packet(bytes);
-				ISerializable received = packet.ReadObject();
-				
-				if (received is ClientJoinRequest) 
-				{
-					// Create new avatar
-					clientState.AvatarId = _nextAvatarId++;
-					clientState.Skin = new Random().Next(0, 1000);
+    //modified processExistingClients to use ClientState
+    private void processExistingClients()
+    {
+        foreach (ClientState clientState in _clientStates.ToList())
+        {
+            NetworkStream stream = clientState.Client.GetStream();
+            if (stream.DataAvailable)
+            {
+                byte[] bytes = StreamUtil.Read(stream);
+                Packet packet = new Packet(bytes);
+                ISerializable received = packet.ReadObject();
 
-					var position = GetValidPosition();
-					clientState.X = position.X;
-					clientState.Y = position.Y;
-					clientState.Z = position.Z;
-					
-					// Send initial info back
-					Send(clientState.Client, new AvatarInfoMessage { 
-						Id = clientState.AvatarId, 
-						Skin = clientState.Skin,
-						Position = clientState.Position
-					});
-					
-					// Broadcast update
-					BroadcastAvatarUpdate();
-				}
-				else if (received is ChatMessage chat) 
-				{
-					chat.SenderId = clientState.AvatarId;
-					Broadcast(chat);
-				}
-			}
-		}
-	}
+                if (received is ClientJoinRequest)
+                {
+                    clientState.AvatarId = _nextAvatarId++;
+                    clientState.Skin = new Random().Next(0, 1000);
+                    var position = GetValidPosition();
+                    clientState.X = position.X;
+                    clientState.Y = position.Y;
+                    clientState.Z = position.Z;
 
-	// > new broadcast helpers
-	private void Broadcast(ISerializable message) 
-	{
-		Packet packet = new Packet();
-		packet.Write(message);
-		byte[] bytes = packet.GetBytes();
-		foreach (var client in _clients) {
-			StreamUtil.Write(client.Client.GetStream(), bytes);
-		}
-	}
+                    Send(clientState.Client, new AvatarInfoMessage
+                    {
+                        Id = clientState.AvatarId,
+                        Skin = clientState.Skin,
+                        X = clientState.X,
+                        Y = clientState.Y,
+                        Z = clientState.Z
+                    });
 
-	private void BroadcastAvatarUpdate() 
-	{
-		var update = new AvatarUpdateMessage();
-		foreach (var client in _clients) {
-			update.Avatars.Add(new AvatarInfoMessage {
-				Id = client.AvatarId,
-				Skin = client.Skin,
-				X = clientState.X,
-				Y = clientState.Y,
-				Z = clientState.Z
-			});
-		}
-		Broadcast(update);
-	}
+                    BroadcastAvatarUpdate();
+                }
+                else if (received is ChatMessage chat)
+                {
+                    chat.SenderId = clientState.AvatarId;
+                    Broadcast(chat);
+                }
+            }
+        }
+    }
 
+    // > send packet
+    private void Send(TcpClient pClient, ISerializable pMessage)
+    {
+        try
+        {
+            Packet packet = new Packet();
+            packet.Write(pMessage);
+            byte[] bytes = packet.GetBytes();
+            StreamUtil.Write(pClient.GetStream(), bytes);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error sending message: {e.Message}");
+        }
+    }
 
-	// > convert loc info for avatarinfoupdate
-	private (float X, float Y, float Z) GetValidPosition() 
-	{
-		float angle = new Random().Next(0, 360) * MathF.PI / 180f;
-		float distance = new Random().Next(0, 10);
-		return (
-			X: MathF.Cos(angle) * distance,
-			Y: 0,
-			Z: MathF.Sin(angle) * distance
-		);
-	}
+    // > new broadcast helpers
+    private void Broadcast(ISerializable message)
+    {
+        Packet packet = new Packet();
+        packet.Write(message);
+        byte[] bytes = packet.GetBytes();
+        foreach (var clientState in _clientStates)
+        {
+            StreamUtil.Write(clientState.Client.GetStream(), bytes);
+        }
+    }
 
+    private void BroadcastAvatarUpdate()
+    {
+        var update = new AvatarUpdateMessage();
+        foreach (var clientState in _clientStates)
+        {
+            update.Avatars.Add(new AvatarInfoMessage
+            {
+                Id = clientState.AvatarId,
+                Skin = clientState.Skin,
+                X = clientState.X,  // Fixed variable name
+                Y = clientState.Y,
+                Z = clientState.Z
+            });
+        }
+        Broadcast(update);
+    }
+
+    // > convert loc info
+    private (float X, float Y, float Z) GetValidPosition()
+    {
+        float angle = new Random().Next(0, 360) * MathF.PI / 180f;
+        float distance = new Random().Next(0, 10);
+        return (MathF.Cos(angle) * distance, 0, MathF.Sin(angle) * distance);
+    }
 }
-
